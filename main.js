68e7e34be0a85e5839e5f95ab7a600720c4d82cc
NOZYNNN1 <adamomok1211@gmail.com>
2024-11-11 09:43:33 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function resetB(){
+    document.getElementById('Forum').reset();
+}
+
+document.getElementById('Sub').addEventListener('click',function(){
+    alert("Selesai");
+});
+document.getElementById('Tentang').addEventListener('click', function(){
+    window.open('Tentang.html','_self')
+})
